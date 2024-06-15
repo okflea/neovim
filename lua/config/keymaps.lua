6a70d2edf697c9c210007065bce5d6bad29171d0
okflea <okflea@plz.dnt>
2024-06-15 08:14:07 +0530

    mini git test(): title

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set({ "n", "x" }, "<leader>ghz", "<Cmd>lua MiniGit.show_at_cursor()<CR>", { desc = "show at cursor" })
+
+vim.keymap.set({ "n", "x" }, "<leader>ghx", "<Cmd>lua MiniGit.show_diff_source()<CR>", { desc = "show diff source" })
+
+vim.keymap.set(
+  { "n", "x" },
+  "<leader>ghc",
+  "<Cmd>lua MiniGit.show_range_history()<CR>",
+  { desc = "show range history" }
+)
